12efed51dee67d4e727f3fd5b5190ff99ee048f3
Abner Oliveira <81240940+abnersolivera@users.noreply.github.com>
2022-11-28 15:32:03 -0300

    Alteração Livro

--- aula_09-09-22/adm/delLivro.php
+++ aula_09-09-22/adm/delLivro.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Bootstrap CSS -->
+    <title>Livro</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
 </head>
 <body>
     <div class="container">
+    <script src="../js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
 </body>
-<script src="../js/jquery-3.5.1.slim.min.js"></script>
-<script src="../js/popper.min.js"></script>
-
 </html>
