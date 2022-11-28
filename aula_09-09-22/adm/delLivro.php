9e8b88601540288ea577c883f64025bf0fac6315
Abner Oliveira <81240940+abnersolivera@users.noreply.github.com>
2022-11-28 15:43:13 -0300

    livros deletes

--- aula_09-09-22/adm/delLivro.php
+++ aula_09-09-22/adm/delLivro.php
@@ -7,8 +7,6 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
 </head>
 <body>
-    <div class="container">
-    <script src="../js/jquery-3.5.1.slim.min.js"></script>
-    <script src="../js/popper.min.js"></script>
+
 </body>
 </html>
